3acda02d350ce5aa3cbc0cb7f07d526db0066b60
Supun Rajitha Epasinghe <supun@plasmo.com.au>
2024-09-24 13:06:58 +0800

    Program.cs changes

--- LabelPrinting/Program.cs
+++ LabelPrinting/Program.cs
@@ -14,45 +14,26 @@ namespace LabelPrinting
         /// </summary>
         [STAThread]
 
-        //        static void Main(string[] args)
-        //        {
-        //#if DEBUG
-        //            //args = new[] { "MenuOptionJobRun" };
-        //            //args = new[] { "MenuOptionPalletLabel" };
-        //#endif
-
-        //            if (args.Length == 0)
-        //                Application.Run(new MainForm(null));
-        //            else if (args.Length ==1)
-        //            {
-        //                string menuOption = args[0];
-        //                Application.EnableVisualStyles();
-        //                Application.SetCompatibleTextRenderingDefault(false);  
-
-
-        //                if (menuOption == "MenuOptionJobRun")
-        //                    Application.Run(new JobRun());
-        //                else if (menuOption == "MenuOptionPalletLabel")
-        //                    Application.Run(new PromptPalletLabelPrint());
-        //            }
-
-        //        }
-
-
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            //string[] args = Environment.GetCommandLineArgs();
 #if DEBUG
-            //args[0] = "AppendApplicationObjects";
+                    //args = new[] { "MenuOptionJobRun" };
+                    //args = new[] { "MenuOptionPalletLabel" };
 #endif
 
-
             if (args.Length == 0)
+                Application.Run(new MainForm(null));
+            else if (args.Length == 1)
             {
-                Application.Run(new PromptPalletLabelPrint());
+                string menuOption = args[0];
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+
+                if (menuOption == "MenuOptionJobRun")
+                    Application.Run(new JobRun());
+                else if (menuOption == "MenuOptionPalletLabel")
+                    Application.Run(new PromptPalletLabelPrint());
             }
             else if (args[0] == "GetFormNames")
             {
@@ -71,6 +52,40 @@ namespace LabelPrinting
             }
 
         }
+
+
+//        static void Main(string[] args)
+//        {
+//            Application.EnableVisualStyles();
+//            Application.SetCompatibleTextRenderingDefault(false);
+
+//            //string[] args = Environment.GetCommandLineArgs();
+//#if DEBUG
+//            //args[0] = "AppendApplicationObjects";
+//#endif
+
+
+//            if (args.Length == 0)
+//            {
+//                Application.Run(new PromptPalletLabelPrint());
+//            }
+//            else if (args[0] == "GetFormNames")
+//            {
+//                ApplicationAccess aa = new ApplicationAccess();
+//                aa.GetFormNames("LabelPrinting");
+//            }
+//            else if (args[0] == "RefreshFormNames")
+//            {
+//                ApplicationAccess aa = new ApplicationAccess();
+//                aa.RefreshFormNames("LabelPrinting");
+//            }
+//            else if (args[0] == "AppendApplicationObjects")
+//            {
+//                ApplicationAccess aa = new ApplicationAccess();
+//                aa.AppendApplicationObjectList("LabelPrinting");
+//            }
+
+//        }
           
         }
     }
