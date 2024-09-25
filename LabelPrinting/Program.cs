98d2a9209597d2269a7ade6b30de83d792bf4f93
David Fletcher-Jones <davidf-j@outlook.com>
2024-09-25 12:26:49 +0800

    1.  command line arguments fixed 2.  ApplicationAccess.dll source path set to Plasmo-sql\packages\ApplicationAccess 3.  Target Platform set to X64

--- LabelPrinting/Program.cs
+++ LabelPrinting/Program.cs
@@ -16,76 +16,41 @@ namespace LabelPrinting
 
         static void Main(string[] args)
         {
-#if DEBUG
-                    //args = new[] { "MenuOptionJobRun" };
-                    //args = new[] { "MenuOptionPalletLabel" };
-#endif
 
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+#if DEBUG
+            //args = new[] { "MenuOptionJobRun" };
+            //args = new[] { "MenuOptionPalletLabel" };
+            args = new[] { "AppendApplicationObjects" };
+#endif
             if (args.Length == 0)
                 Application.Run(new MainForm(null));
             else if (args.Length == 1)
             {
                 string menuOption = args[0];
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-
 
                 if (menuOption == "MenuOptionJobRun")
                     Application.Run(new JobRun());
                 else if (menuOption == "MenuOptionPalletLabel")
                     Application.Run(new PromptPalletLabelPrint());
+                else if (menuOption == "GetFormNames")
+                {
+                    ApplicationAccess aa = new ApplicationAccess();
+                    aa.GetFormNames("LabelPrinting");
+                }
+                else if (menuOption == "RefreshFormNames")
+                {
+                    ApplicationAccess aa = new ApplicationAccess();
+                    aa.RefreshFormNames("LabelPrinting");
+                }
+                else if (menuOption == "AppendApplicationObjects")
+                {
+                    ApplicationAccess aa = new ApplicationAccess();
+                    aa.AppendApplicationObjectList("LabelPrinting");
+                }
             }
-            else if (args[0] == "GetFormNames")
-            {
-                ApplicationAccess aa = new ApplicationAccess();
-                aa.GetFormNames("LabelPrinting");
-            }
-            else if (args[0] == "RefreshFormNames")
-            {
-                ApplicationAccess aa = new ApplicationAccess();
-                aa.RefreshFormNames("LabelPrinting");
-            }
-            else if (args[0] == "AppendApplicationObjects")
-            {
-                ApplicationAccess aa = new ApplicationAccess();
-                aa.AppendApplicationObjectList("LabelPrinting");
-            }
-
-        }
-
-
-//        static void Main(string[] args)
-//        {
-//            Application.EnableVisualStyles();
-//            Application.SetCompatibleTextRenderingDefault(false);
-
-//            //string[] args = Environment.GetCommandLineArgs();
-//#if DEBUG
-//            //args[0] = "AppendApplicationObjects";
-//#endif
-
-
-//            if (args.Length == 0)
-//            {
-//                Application.Run(new PromptPalletLabelPrint());
-//            }
-//            else if (args[0] == "GetFormNames")
-//            {
-//                ApplicationAccess aa = new ApplicationAccess();
-//                aa.GetFormNames("LabelPrinting");
-//            }
-//            else if (args[0] == "RefreshFormNames")
-//            {
-//                ApplicationAccess aa = new ApplicationAccess();
-//                aa.RefreshFormNames("LabelPrinting");
-//            }
-//            else if (args[0] == "AppendApplicationObjects")
-//            {
-//                ApplicationAccess aa = new ApplicationAccess();
-//                aa.AppendApplicationObjectList("LabelPrinting");
-//            }
-
-//        }
-          
         }
     }
+}
